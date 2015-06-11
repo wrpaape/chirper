39ee7ad1b498bd3dc4d67b0ce2f721ba1a78d130
Reid Paape <wrpaape@gmail.com>
2015-06-11 08:02:51 -0500

    trying to link show user page to show_chirps/comments pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def show_chirps
+    @chirps = User.find(params[:id]).chirps
+  end
+
+  def show_comments
+    @comments = User.find(params[:id]).comments
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
