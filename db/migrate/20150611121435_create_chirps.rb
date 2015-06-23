fb8b6df57441fb54d27a032d7164a896e964c0f7
Reid Paape <wrpaape@gmail.com>
2015-06-23 12:25:39 -0500

    added home posts and about react components

--- db/migrate/20150611121435_create_chirps.rb
+++ db/migrate/20150611121435_create_chirps.rb
@@ -3,7 +3,7 @@ class CreateChirps < ActiveRecord::Migration
     create_table :chirps do |t|
       t.string :title, default: "", null: false
       t.string :body, default: "", null: false
-      t.string :topic, default: ""
+      t.string :topic, default: "#misc"
       t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps null: false
